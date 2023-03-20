fd1ff9fa9243f2ac390bcafb4631181011807a71
medlinnei <87272001+medlinnei@users.noreply.github.com>
2023-03-20 12:12:34 +0200

    Update chain.php

--- Behavioral Patterns/chain.php
+++ Behavioral Patterns/chain.php
@@ -8,74 +8,44 @@
  * Это может быть полезно, например, для обработки ошибок в программе,
  * когда необходимо найти объект, который может обработать конкретную ошибку.
  */
-abstract class Handler
-{
-    private ?Handler $successor;
+<?php
 
-    /**
-     * @param Handler $successor
-     */
-    public function __construct(?Handler $successor)
+abstract class Chain
+{
+    protected $binaryCode;
+    public function setBinaryCode($binaryCode)
     {
-        $this->successor = $successor;
+        $this->binaryCode = $binaryCode;
     }
-    final public function handle(TaskInterface $task)
+    abstract public function getBinaryCode($request);
+}
+class BinaryOne extends Chain
+{
+    public function getBinaryCode($request)
     {
-        $proceed = $this->processing($task);
-        if($proceed === null and $this->successor){
-            $proceed = $this->successor->handle($task);
+        if ($request == "BinaryOne"){
+            echo "BinaryOne";
+        }elseif ($this->binaryCode){
+            $this->binaryCode->getBinaryCode($request);
         }
-        return $proceed;
     }
-
-    abstract public function processing(TaskInterface $task): ?array;
 }
-
-interface TaskInterface
+class BinaryTwo extends Chain
 {
-    public function getArray(): array;
-}
 
-class DevTask implements TaskInterface
-{
-    private array $arr = [1, 2, 3];
-    public function getArray(): array
+    public function getBinaryCode($request)
     {
-        return $this->arr;
+        if ($request == "BinaryOne")
+        {
+            echo "BinaryOne";
+        }elseif ($this->binaryCode){
+            $this->binaryCode->getBinaryCode($request);
+        }
     }
 }
 
-class Junior extends Handler
-{
+$binaryCodeOne = new BinaryOne();
+$binaryCodeTwo = new BinaryTwo();
+$binaryCodeOne->setBinaryCode($binaryCodeTwo);
 
-    public function processing(TaskInterface $task): ?array
-    {
-        return $task->getArray();
-    }
-}
-
-class Middle extends Handler
-{
-
-    public function processing(TaskInterface $task): ?array
-    {
-        return null;
-    }
-}
-
-class Senior extends Handler
-{
-
-    public function processing(TaskInterface $task): ?array
-    {
-        return null;
-    }
-}
-
-$task = new DevTask();
-
-$senior = new Senior(null);
-$middle = new Middle($senior);
-$junior = new Junior($middle);
-
-var_dump($junior->handle($task));
+$binaryCodeOne->getBinaryCode("BinaryOne");
