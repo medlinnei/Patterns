f7ca68ab997b1e53b05981e3849432fdffa984eb
medlinnei <87272001+medlinnei@users.noreply.github.com>
2023-03-21 22:51:16 +0200

    Update abstract_factory.php

--- Creational Patterns/abstract_factory.php
+++ Creational Patterns/abstract_factory.php
@@ -10,95 +10,72 @@
  * которые взаимодействуют друг с другом и отвечают определенным требованиям,
  * но не зависят от конкретных классов этих объектов.
  */
-interface AbstractFactory
-{
-    public static function makeDeveloperWorker(): DeveloperWorker;
-    public static function makeDesignerWorker(): DesignerWorker;
+<?php
+
+// Абстрактный продукт A
+interface AbstractProductA {
+    public function getProduct(): string;
 }
 
-class OutsourceWorkerFactory implements AbstractFactory
-{
-
-    public static function makeDeveloperWorker(): DeveloperWorker
-    {
-        return new OutsourceDeveloperWorker();
-    }
-
-    public static function makeDesignerWorker(): DesignerWorker
-    {
-        return new OutsourceDesignerWorker();
+// Конкретный продукт A1
+class ConcreteProductA1 implements AbstractProductA {
+    public function getProduct(): string {
+        return "Product A1";
     }
 }
 
-class NativeWorkerFactory implements AbstractFactory
-{
-
-    public static function makeDeveloperWorker(): DeveloperWorker
-    {
-        return new NativeDeveloperWorker();
-    }
-
-    public static function makeDesignerWorker(): DesignerWorker
-    {
-        return new NativeDesignerWorker();
+// Конкретный продукт A2
+class ConcreteProductA2 implements AbstractProductA {
+    public function getProduct(): string {
+        return "Product A2";
     }
 }
 
-interface Worker
-{
-    public function work();
-}
-interface DeveloperWorker extends Worker
-{
-
+// Абстрактный продукт B
+interface AbstractProductB {
+    public function getProduct(): string;
 }
 
-interface DesignerWorker extends Worker
-{
-
-}
-
-class NativeDeveloperWorker implements DeveloperWorker
-{
-
-    public function work()
-    {
-        print_r("I`m developing as native");
+// Конкретный продукт B1
+class ConcreteProductB1 implements AbstractProductB {
+    public function getProduct(): string {
+        return "Product B1";
     }
 }
 
-class OutsourceDeveloperWorker implements DeveloperWorker
-{
-
-    public function work()
-    {
-        print_r("I`m developing as outsource");
+// Конкретный продукт B2
+class ConcreteProductB2 implements AbstractProductB {
+    public function getProduct(): string {
+        return "Product B2";
     }
 }
 
-class NativeDesignerWorker implements DesignerWorker
+// Фабрика продуктов
+class ProductFactory
 {
-
-    public function work()
+    public static function createProductA1(string $type): AbstractProductA
     {
-        print_r("I`m designer as native");
+        return new ConcreteProductA1();
+    }
+
+    public static function createProductA2(string $type): AbstractProductA
+    {
+        return new ConcreteProductA2();
+    }
+
+    public static function createProductB1(string $type): AbstractProductB
+    {
+        return new ConcreteProductB1();
+    }
+    public static function createProductB2(string $type): AbstractProductB
+    {
+        return new ConcreteProductB2();
     }
 }
 
-class OutsourceDesignerWorker implements DesignerWorker
-{
 
-    public function work()
-    {
-        print_r("I`m designer as outsource");
-    }
-}
+// Клиентский код
+$productA = ProductFactory::createProductA1('A1');
 
-$nativeDeveloper = NativeWorkerFactory::makeDeveloperWorker();
-$outsourceDeveloper = OutsourceWorkerFactory::makeDeveloperWorker();
-$nativeDesigner = NativeWorkerFactory::makeDesignerWorker();
-$outsourceDesigner = OutsourceWorkerFactory::makeDesignerWorker();
-
-$nativeDesigner->work();
-
+echo $productA->getProduct() . "\n";
 
